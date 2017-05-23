29170d7adf87f630fc3cd093d3a03fe035664e94
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-23 00:09:15 -0300

    version restaurar fotos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,41 @@
 
 (function salir(){
 	var pandas = Array.from(document.querySelectorAll('.panda span'));
+	var boton = document.getElementById('recargar');
+	var img = Array.from(document.querySelectorAll('.imagenes div'));
 	
 	pandas.forEach(function(element){
 		element.addEventListener('click', function(){
 			this.parentElement.classList.add('salir');
 		});
 	});
+	
+	function borrar(){
+        pandas.forEach(function(e){
+            e.parentElement.classList.remove('salir');
+        });
+    }
+    
+    boton.addEventListener('click', borrar);
+	/*boton.addEventListener('click', function(){
+		pandas.forEach(function(e){
+			e.parentElement.classList.remove('salir');
+		});
+	});
+  */
+	// boton.addEventListener("click", function(){
+ //        pandas.forEach(function(ele){
+ //            ele.parentElement.classList.remove("salir");
+ //        });
+ //    });
 })()
+
+var botonUno = document.getElementById('parrafo1');
+var textoUno = document.getElementById('uno');
+
+botonUno.addEventListener('click', function(){
+	textoUno.classList.remove('salir');
+});
+botonUno.addEventListener('click',textoUno);
+
 
